2bd98aca28c9cda374b1334a7c9ec610870f7d57
Frank Congson <fcongson@gmail.com>
2025-04-30 09:43:03 +1200

    Clean up time

--- src/app/page.js
+++ src/app/page.js
@@ -6,20 +6,21 @@ import { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
-  const [time, setTime] = useState(new Date());
+  const [time, setTime] = useState("");
+  const [isMounted, setIsMounted] = useState(false);
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime(new Date());
-    }, 1000);
-
+    setIsMounted(true);
+    const updateTime = () => setTime(new Date().toLocaleTimeString());
+    updateTime();
+    const interval = setInterval(updateTime, 1000);
     return () => clearInterval(interval);
   }, []);
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>{time.toLocaleTimeString()}</h1>
+        <h1 className={styles.time}>{!isMounted ? null : time}</h1>
       </main>
       <footer className={styles.footer}>
         <div className={styles.ctas}>
